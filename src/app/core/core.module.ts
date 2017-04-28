ba1c39259d617c76851395d23daa2d8f41d7734e
Henry Leon <helg18@gmail.com>
2017-04-27 22:26:27 -0400

    Formando el esqueleto de la aplicacion

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShellComponent } from './shell/shell.component';
+import { TopBarComponent } from './shell/top-bar/top-bar.component';
+import { MainContentComponent } from './shell/main-content/main-content.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ShellComponent],
-  exports:[ShellComponent]
+  declarations: [
+			  		ShellComponent, 
+			  		TopBarComponent, 
+			  		MainContentComponent
+		  		],
+  exports:[
+  			ShellComponent
+  			]
 })
 export class CoreModule { }
